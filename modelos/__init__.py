b1cd755d14b4cbd036486d6292b40f404249ba8f
Emiliano <ebgroh@ful.unl.edu.ar>
2025-06-10 19:02:39 -0300

    actualizado

---
+++ modelos/__init__.py
@@ -0,0 +1,7 @@
+from .regresion_lineal import RegresionLineal
+from .regresion_lineal_simple import RegresionLinealSimple
+from .regresion_lineal_multiple import RegresionLinealMultiple
+from .regresion_logistica import RegresionLogistica
+
+__all__ = ["RegresionLineal", "RegresionLinealSimple",
+           "RegresionLinealMultiple", "RegresionLogistica"]
