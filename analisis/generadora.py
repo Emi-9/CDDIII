f62deaa6737259f5655d9c949d0b8d7899f4b61e
Emiliano <ebgroh@ful.unl.edu.ar>
2025-06-10 15:10:11 -0300

    corregida y documentada

--- analisis/generadora.py
+++ analisis/generadora.py
@@ -1,11 +1,14 @@
 # from statsmodels.nonparametric.kde import KDEUnivariate
 import numpy as np
+from typing import Any
 from scipy.stats import norm, uniform
 
 
 class GeneradoraDeDatos:
     '''
-    Documentar
+    Primero se instancia (toma la cantidad a generar), luego:
+    - teorica => exacta
+    - azar => muestral
     '''
 
     def __init__(self, q: int) -> None:
@@ -14,40 +17,55 @@ class GeneradoraDeDatos:
     def __main__(self):
         pass
 
-    def teorica(self, x, mu, sigma, type):
+    def teorica(self, x: list[Any] | np.ndarray[Any, Any],
+                mu: float = 0, sigma: float = 1,
+                type: str = "normal") -> np.ndarray[Any, Any]:
         '''
-        Documentar
+        - x => cualquier iterable que contenga los puntos donde
+               se va a evaluar.
+        - mu => punto sobre el cual va a estar centrada la
+                distribucion (media).
+        - sigma => amplitud de la distribucion (varianza).
+        - type => normal, uniforme o BS.
         '''
         if type == "normal":
             return norm.pdf(x, mu, sigma)
         elif type == "uniforme":
             return uniform.pdf(x, mu, sigma)
         elif type == "BS":
-            print("es una distribucion inventada por las profesora"
-                  "no se como es la teorica")
+            term1 = (1/2) * norm.pdf(x, loc=0, scale=1)
+            term_mid = [norm.pdf(x, loc=j/2-1, scale=1/10) for j in range(5)]
+            term2 = (1/10) * sum(term_mid)
+            return term1 + term2
         else:
             print("Tipo INCORRECTO")
+            return np.array([])  # Callate pylance
 
-    def azar(self, mu, sigma, type):  # TODO: completar
+    def azar(self, mu: float = 0, sigma: float = 1,
+             type: str = "normal") -> np.ndarray[Any, Any] | int:
+        # Segun pylance existe la posibilidad de que retorne int
+        # Al menos asi se calla
         '''
-        Documentar
+        - x => cualquier iterable que contenga los puntos donde
+               se va a evaluar.
+        - mu => punto sobre el cual va a estar centrada la
+                distribucion (media).
+        - sigma => amplitud de la distribucion (varianza).
+        - type => normal, uniforme o BS.
         '''
-        res = uniform.rvs(loc=mu, scale=sigma, size=self.q)
-        return res
-
-    def norm(self, mu, sigma):  # combinar con la de azar
-        res = norm.rvs(loc=mu, scale=sigma, size=self.q)
-        return res
-
-    def BS(self):
-        '''
-        Bart Simpson.
-        '''
-        u = np.random.uniform(size=(self.q))
-        y = u.copy()
-        ind = np.where(u > 0.5)[0]
-        y[ind] = np.random.normal(0, 1, size=len(ind))
-        for j in range(5):
-            ind = np.where((u > j * 0.1) & (u <= (j+1) * 0.1))[0]
-            y[ind] = np.random.normal(j/2 - 1, 1/10, size=len(ind))
-        return y
+        if type == "normal":
+            return norm.rvs(loc=mu, scale=sigma, size=self.q)
+        elif type == "uniforme":
+            return uniform.rvs(loc=mu, scale=sigma, size=self.q)
+        elif type == "BS":
+            u = np.random.uniform(size=(self.q))
+            y = u.copy()
+            ind = np.where(u > 0.5)[0]
+            y[ind] = np.random.normal(0, 1, size=len(ind))
+            for j in range(5):
+                ind = np.where((u > j * 0.1) & (u <= (j+1) * 0.1))[0]
+                y[ind] = np.random.normal(j/2-1, 1/10, size=len(ind))
+            return y
+        else:
+            print("Tipo INCORRECTO")
+            return np.array([])  # Callate pylance
