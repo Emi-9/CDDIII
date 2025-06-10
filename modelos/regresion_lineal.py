ee9a7895a5f51f141ddcb5713cbb628d3bff0fb7
Emiliano <ebgroh@ful.unl.edu.ar>
2025-06-10 18:36:24 -0300

    pasado de collab a git

--- modelos/regresion_lineal.py
+++ modelos/regresion_lineal.py
@@ -1,169 +1,187 @@
 import numpy as np
 import statsmodels.api as sm
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+from scipy.stats import shapiro
+from statsmodels.stats.diagnostic import het_breuschpagan
+from typing import Any
 from analisis import AnalisisDescriptivo
 
 
 class RegresionLineal:
-    """
-    Clase que permite el ajuste de un modelo de Regresion Lineal, que puede
-    ser Regresion Lineal Simple y Regresion Lineal Multiple.
-    Ambas clases depende de esta clase general.
-    """
-    def __init__(self, x, y):
-        # x = variables predictora/s
-        # y = variable respuesta
+    '''
+    Documentar.
+    '''
+
+    def __init__(self, x: np.ndarray[Any, Any],
+                 y: np.ndarray[Any, Any]) -> None:
         self.x = x
         self.y = y
         self.resultado = None
 
+    def __main__(self):
+        pass
+
     def ajustar_modelo(self):
-        """
+        '''
         Se ajuta el modelo de Regresión.
-        """
-        # se arma la matriz de diseño agregando la columna de unos
-        X = sm.add_constant(self.x)
-        # se estima el modelo de regresión lineal
-        modelo = sm.OLS(self.y, X)
-        self.resultado = modelo.fit()
-        return self.resultado
+        '''
+        X = sm.add_constant(self.x)  # TODO: Agregar caso len = 1
+        modelo = sm.OLS(self.y, X).fit()
+        self.resultado = modelo
+        return modelo
 
     def parametros_modelo(self):
-        """
+        '''
         Retorna las estimaciones de los betas del modelo.
-        """
+        '''
         if self.resultado is None:
-            print("Falta ajustar el modelo, usar ajustar_modelo()")
+            print("Falta ajustar el modelo")
+            return None
         else:
-            parametros = self.resultado.params
-            return parametros
+            return self.resultado.params
 
     def ajustado_y(self):
-        """Calula el valor predicho a partir del modelo ajustado
-          de regresion lineal.
-        """
+        '''
+        Calula el valor predicho a partir del modelo ajustado
+        de regresion lineal.
+        '''
         if self.resultado is None:
-          print("Falta ajustar el modelo, usar ajustar_modelo()")
+            print("Falta ajustar el modelo")
+            return None
         else:
-          y_pred = self.resultado.fittedvalues
-          return y_pred
+            return self.resultado.fittedvalues
 
     def residuos(self):
-        """ Calcula los residuos de entre los valores reales (y)
-            y los valores dados por la recta d eminimos cuadrados (y_sombrero)
-        """
+        '''
+        Calcula los residuos de entre los valores reales (y)
+        y los valores dados por la recta d eminimos cuadrados (y_sombrero)
+        '''
         if self.resultado is None:
-          print("Falta ajustar el modelo, usar ajustar_modelo()")
+            print("Falta ajustar el modelo")
+            return None
         else:
-          residuos = self.resultado.resid
-          return residuos
+            return self.resultado.resid
 
     def estim_varianza_del_error(self):
-        """Calcula la estimacion de la varianza del error
-        """
+        '''
+        Calcula la estimacion de la varianza del error.
+        '''
         n = len(self.x)
         resid = self.residuos()
-        var = np.sum( resid**2 ) / (n-2)
+        var = np.sum(resid**2) / (n-2)
         return var
 
     def r_cuadrado(self):
-        """ Calcula (R^2) el coeficiente de determinación y, es una medida de la
-            proporción de la variabilidad que explica el modelo ajustado.
-            valores de  R^2  cercanos a 1 son valores deseables para una buena
-            calidad del ajuste.
-        """
+        '''
+        Calcula (R^2) el coeficiente de determinación y, es una medida de la
+        proporción de la variabilidad que explica el modelo ajustado.
+        valores de  R^2  cercanos a 1 son valores deseables para una buena
+        calidad del ajuste.
+        '''
         if self.resultado is None:
-          print("Falta ajustar el modelo, usar ajustar_modelo()")
+            print("Falta ajustar el modelo")
         else:
-          r_squared = self.resultado.rsquared
-          return r_squared
+            r_squared = self.resultado.rsquared
+            return r_squared
 
     def r_ajustado(self):
-        """Calcula el R² ajustado, es una corrección de  R^2  para permitir
-          la comparación de modelos con distinta cantidad de regresoras.
-        """
+        '''
+        Calcula el R² ajustado, es una corrección de  R^2  para permitir
+        la comparación de modelos con distinta cantidad de regresoras.
+        '''
         if self.resultado is None:
-          print("Falta ajustar el modelo, usar ajustar_modelo()")
+            print("Falta ajustar el modelo")
         else:
-          adjusted_r_squared = self.resultado.rsquared_adj
-          return adjusted_r_squared
+            adjusted_r_squared = self.resultado.rsquared_adj
+            return adjusted_r_squared
 
     def supuesto_normalidad(self):
-      """Se verifica el supuesto de normalidad de los residuos, de manera
-         grafica usando qqplot y de manera analítica usando shapiro test, usando
-         el p-valor.
-      """
-      residuo = self.residuos()
-      # grafica:
-      rg = AnalisisDescriptivo(residuo)
-      #miqqplot(residuo)
-      rg.miqqplot()
+        '''
+        Se verifica el supuesto de normalidad de los residuos, de manera
+        grafica usando qqplot y de manera analítica usando shapiro test, usando
+        el p-valor.
+        '''
+        residuo = self.residuos()
+        # grafica:
+        rg = AnalisisDescriptivo(residuo)
+        # miqqplot(residuo)
+        rg.miqqplot()
 
-      # test de normalidad:
-      stat, p_valor1 = shapiro(residuo)
-      print("\nValor p normalidad:", p_valor1)
+        # test de normalidad:
+        stat, p_valor1 = shapiro(residuo)
+        print("\nValor p normalidad:", p_valor1)
 
     def supuestos_homocedasticidad(self):
-      """Se verifica el supuesto de homocedasticidad de los residuos, de
-          manera grafica y analítica por medio del p-valor.
-      """
-      # Homocedasticidad grafico
-      predichos = self.ajustado_y()
-      residuo = self.residuos()
+        '''
+        Se verifica el supuesto de homocedasticidad de los residuos, de
+        manera grafica y analítica por medio del p-valor.
+        '''
+        # Homocedasticidad grafico
+        predichos = self.ajustado_y()
+        residuo = self.residuos()
 
-      plt.scatter(predichos, residuo, marker="o", c='blue', s=30)
-      plt.axhline(y=0, color='r', linestyle='--')  # Línea horizontal en y=0 para facilitar la visualización de los residuos
-      plt.xlabel('Valores predichos')
-      plt.ylabel('Residuos')
-      plt.title('Gráfico de Residuos vs. Valores Predichos')
-      plt.show()
-      # Homocedasticidad test:
-      X = sm.add_constant(self.x) # matriz de diseño
-      bp_test = het_breuschpagan(residuo, X)# X es la matriz de diseño
-      bp_value = bp_test[1]
-      print("\nValor p Homocedasticidad:", bp_value)
+        plt.scatter(predichos, residuo, marker="o", c="blue", s=30)
+        plt.axhline(y=0, color="r", linestyle="--")
+        # Línea horizontal en y=0 para facilitar la visualización
+        # de los residuos
+        plt.xlabel("Valores predichos")
+        plt.ylabel("Residuos")
+        plt.title("Gráfico de Residuos vs. Valores Predichos")
+        plt.show()
+        # Homocedasticidad test:
+        X = sm.add_constant(self.x)  # matriz de diseño
+        bp_test = het_breuschpagan(residuo, X)  # X es la matriz de diseño
+        bp_value = bp_test[1]
+        print("\nValor p Homocedasticidad:", bp_value)
 
     def int_confianza_betas(self, alfa):
-        """funcion inicial: int_confianza_beta1(alfa, beta_1, var_estimada, t_crit)
-        Calcula el intervalor de confianza para beta_1, a partir de un alfa (nivel
-        de significacion) dado.
-        """
+        '''
+        funcion inicial: int_confianza_beta1(alfa, beta_1,
+        var_estimada, t_crit)
+        Calcula el intervalor de confianza para beta_1, a partir de un alfa
+        (nivel de significacion) dado.
+        '''
         if self.resultado is None:
-          print("Falta ajustar el modelo, usar ajustar_modelo()")
+            print("Falta ajustar el modelo")
         else:
-          IC = self.resultado.conf_int(alpha = alfa)
-          print(f"Los Intervalos de confianza para los estimadores de beta son: {IC}")
+            IC = self.resultado.conf_int(alpha=alfa)
+            print("Los Intervalos de confianza para los"
+                  f"estimadores de beta son: {IC}")
 
     def p_valor_betas(self, b_i=0, i=1):
-        """Es una funcion que retorna el p-valor de un test de hipotesis:
-                          H_0: beta_i = k vs H_1 beta_i != k
-            b_i: es el numero k sobre el cual se quiere hacer el test. Por
-            default es 0.
-            i: es el indice del beta que se quiere testear, es un natural i
-            (i = 0, ..., n). Por default es 1.
-        """
+        '''
+        Es una funcion que retorna el p-valor de un test de hipotesis:
+        H_0: beta_i = k vs H_1 beta_i != k
+        b_i: es el numero k sobre el cual se quiere hacer el test. Por
+        default es 0.
+        i: es el indice del beta que se quiere testear, es un natural i
+        (i = 0, ..., n). Por default es 1.
+        '''
         if self.resultado is None:
-          print("Falta ajustar el modelo, usar ajustar_modelo()")
+            print("Falta ajustar el modelo")
         else:
-          res = self.resultado
-          SE_est = res.bse
-          coef_xi = res.params[i]
-          # valor de t observado:
-          t_obs = (coef_xi - b_i)/SE_est[i]
+            res = self.resultado
+            SE_est = res.bse
+            coef_xi = res.params[i]
+            # valor de t observado:
+            t_obs = (coef_xi - b_i) / SE_est[i]
 
-          # el pvalor:
-          X = res.model.exog # para recuperar la matriz de diseño del modelo
-          grados_libertad = len(X[:, i]) - 2
-          p_valor = 2 * stats.t.sf(abs(t_obs), df = grados_libertad)
+            # el pvalor:
+            X = res.model.exog  # para recuperar la matriz de diseño del modelo
+            grados_libertad = len(X[:, i]) - 2
+            p_valor = 2 * stats.t.sf(abs(t_obs), df=grados_libertad)
 
-          return p_valor
+            return p_valor
 
     def resumen_grafico(self, z):
-        """ Grafico de dispersion de una variable cuantitativa predictora vs
-            respuesta.
-            z es la variable cuantitativa predictora que se quiere graficar.
-        """
-        plt.scatter(z, self.y, marker="o", c='blue', s=30)
-        plt.xlabel('Variable Predictora')
-        plt.ylabel('Variable Respuesta')
-        plt.title('Gráfico de Dispersion: Var.Predict. vs. Var. Respuesta')
+        '''
+        Grafico de dispersion de una variable cuantitativa predictora vs
+        respuesta.
+        z es la variable cuantitativa predictora que se quiere graficar.
+        '''
+        plt.scatter(z, self.y, marker="o", c="blue", s=30)
+        plt.xlabel("Variable Predictora")
+        plt.ylabel("Variable Respuesta")
+        plt.title("Gráfico de Dispersion: Var.Predict. vs. Var. Respuesta")
         plt.show()
---
+++ modelos/regresion_lineal_simple.py
@@ -0,0 +1,138 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+import scipy.stats as stats
+from .regresion_lineal import RegresionLineal
+
+
+class RegresionLinealSimple(RegresionLineal):
+    """
+    Clase regresion Lineal Simple, hereda funciones de Regresion Lineal.
+    """
+
+    def __init__(self, x, y):
+        super().__init__(x, y)
+
+    def estimacion_betas(self):
+        """
+        Retorna una tupla (b_0, b_1) de los estimadores de beta_0 y beta_1,
+        usando minimos cuadrados.
+        """
+        x_media = np.mean(self.x)
+        y_media = np.mean(self.y)
+        numerador = np.sum((self.x - x_media) * (self.y - y_media))
+        denominador = np.sum((self.x - x_media)**2)
+        b_1 = numerador / denominador
+        b_0 = y_media - b_1 * x_media
+        return (b_0, b_1)
+
+    def graf_scatter_recta(self):
+        """
+        Grafica los puntos de la variable predictora vs vaiable de respuesta.
+        Ademas grafica la recta de minimos cuadrados.
+        """
+        # el regresor lineal:
+        b_0, b_1 = self.estimacion_betas()
+        y_pred = b_0 + b_1 * self.x
+        # el grafico:
+        plt.scatter(self.x, self.y, marker="o", c='blue', label='Datos', s=30)
+        plt.plot(self.x, y_pred, linestyle='--', color='red', label='Recta estimada')
+        plt.legend()
+        plt.xlabel("variable predictora")
+        plt.ylabel("variable respuesta")
+        plt.title('')
+        plt.show()
+
+    def y_predict_x_new(self, x_new):
+        """
+        Retorna el valor de un y_predicho del modelo de regresion
+        a partir de un nuevo valor de x: x_new.
+        """
+        if self.resultado is None:
+            print("Falta ajustar el modelo")
+
+        else:
+            res = self.resultado
+            # Crear la matriz de diseño con el nuevo punto de predicción
+            X_new = sm.add_constant(np.array([[x_new]]))
+            prediccion = res.predict(X_new)
+            return prediccion
+
+    def t_obs_b1(self, b1=0):
+        """
+        Funcion que calcula del t observado para determinar el sgte. test
+        H_0: beta_1 = 0 vs H_1: beta_1 != 0
+        Donde b es el valor que toma beta_1, por defecto es 0, porque se evalua
+        el test de arriba.
+        Pero b=1 si se evaluara el test: H_0: beta_1 = 1 vs H_1: beta_1 != 1.
+        """
+        if self.resultado is None:
+            print("Falta ajustar el modelo, usar ajustar_modelo()")
+        else:
+            res = self.resultado
+            coef_x = res.params[1]
+            # error estándar estimado para el estimador de los betas
+            SE_est = res.bse
+
+            # valor de t observado:
+            t_obs = (coef_x - b1) / SE_est[1]  # SE_est[1] es el error estandar
+            # estimado para el estimador beta_1
+            return t_obs
+
+    def reg_rechazo_b1(self, alfa):
+        """
+        Funcion que muestra la region de rechazo para la hipotesis nula H_0
+        a favor  de aceptar la hipotesis alternativa H_1.
+        """
+        # Completar
+        # alfa = 0.05
+        grados_libertad = len(self.x) - 2
+        t_crit = stats.t.ppf(1 - (alfa/2), df=grados_libertad)
+        print(f"(-inf, {-t_crit}) U ({t_crit}, inf)")
+
+    def p_valor_beta(self, b1=0):
+        """
+        calcula el p-valor para evaluar el test de hipotesis:
+        H_0: beta_1 = 0 vs H_1: beta_1 != 0
+        """
+        t_observado = self.t_obs_b1(b1)
+        grados_libertad = len(self.x) - 2
+        p_valor = 2 * stats.t.sf(abs(t_observado), df=grados_libertad)
+        return p_valor
+
+    def int_confianza_betas(self, alfa):
+        """
+        Funcion inicial: int_confianza_beta1(alfa, beta_1, var_estimada, t_crit)
+        Calcula el intervalor de confianza para beta_1, a partir de un alfa (nivel
+        de significacion) dado.
+        """
+
+        if self.resultado is None:
+            print("Falta ajustar el modelo, usar ajustar_modelo()")
+        else:
+            IC = self.resultado.conf_int()
+            print(f"Intervalo de confianza para beta_1 es: {IC[1]}")
+
+    def int_prediccion_y(self, metodo, x_new, alfa):
+        """
+        Calcula el intervalo de predccion de una Y, a partir de una x_new,
+        usando los metodos:
+        - Metodo 1:  Construir un intervalo de confianza para el valor esperado de
+                     Y para un valor particular de  X , por ejemplo  x0 :  E(Y|X=x0)
+        - Metodo 2: Construir un intervalo de predicción de  Y  para un valor
+                    particular de  X , por ejemplo  x0 :  Y0 .
+                    se obtiene un intervalo de confianza/prediccion de nivel (1-alfa)
+        """
+        if self.resultado is None:
+            print("Falta ajustar el modelo, usar ajustar_modelo()")
+        else:
+            res = self.resultado
+            # Crear la matriz de diseño con el nuevo punto de predicción:
+            X_new = sm.add_constant(np.array([[1, x_new]]))
+            prediccion = res.get_prediction(X_new)
+
+            if metodo == 1:
+                return prediccion.conf_int(alpha=alfa, obs=False)
+
+            elif metodo == 2:
+                return prediccion.conf_int(obs=True, alpha=alfa)
