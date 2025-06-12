a1b691a8d9f1625162d4e4dc25e0138441a57fc1
Emiliano <ebgroh@ful.unl.edu.ar>
2025-06-11 23:52:35 -0300

    actualizado

--- modelos/regresion_logistica.py
+++ modelos/regresion_logistica.py
@@ -42,51 +42,48 @@ class RegresionLogistica:
     # TODO: anotaciones
     def separar_data_train_test(self, seed=10, ptje_test=0.20) -> Any:
         """
-        Funcion que separa data, de manera aleatoria, en set de train y test.
-        seed: es la semilla, por default es 10.
-        ptje_test: Default= 0.20. Es el valor entre 0 y 1, es la proporcion
-        que se quiere dejar para el conjunto test, tomado de self.data.
+        Separa los datos al azar (train y test):
+        - seed: semilla para replicabilidad.
+        - ptje_test: valor entre 0 y 1, es la proporcion
+                     que deja para el conjunto test (tomado de self.data).
         """
         random.seed(seed)
         cant_filas_extraer = int(self.data.shape[0] * ptje_test)
-        # Crear un vector de números aleatorios entre 0 y len(data)
         cuales = random.sample(range(int(self.data.shape[0])),
                                cant_filas_extraer)
-        # datos train:
         self.data_train = self.data.drop(cuales)
-        # datos test:
         self.data_test = self.data.iloc[cuales]
 
         return self.data_test, self.data_train
 
     # TODO: anotaciones
-    def ajustar_modelo(self, x_train, y_train, x_test, y_test):
+    def ajustar_modelo(self, x_train, y_train, x_test, y_test) -> Any:
         """
-        Se ajusta el modelo de Regresión Logistica.
+        Ajusta el modelo.
         """
         self.x_train = x_train
         self.y_train = y_train
         self.x_test = x_test
         self.y_test = y_test
-        # se arma la matriz de diseño agregando la columna de unos
+
         X = sm.add_constant(self.x_train)
-        # se estima el modelo de Regresión Logistica
-        modelo = sm.Logit(self.y_train, X)  # esto es lo nuevo
+        modelo = sm.Logit(self.y_train, X)
         self.resultado = modelo.fit()
+
         return self.resultado
 
     # TODO: anotaciones
-    def parametros_modelo(self):
+    def parametros_modelo(self) -> Any:
         """
-        Retorna las estimaciones de los betas del modelo.
+        Retorna las estimaciones de los betas.
         """
-        if self.resultado is None:
-            print("Falta ajustar el modelo, usar ajustar_modelo()")
+        if self.resultado is not None:
+            return self.resultado.params
         else:
-            parametros = self.resultado.params
-            return parametros
+            raise RuntimeError("AJUSTAR EL MODELO ANTES")
 
-    def ajustados_y(self, prob=0.5):
+    # TODO: anotaciones.
+    def ajustados_y(self, prob=0.5) -> Any:
         """
         Calula el valor predicho a partir del modelo ajustado
         de regresion lineal.
@@ -95,18 +92,16 @@ class RegresionLogistica:
         Retorna una tupla: y_ajustado_binary, ajust_y_prob
         """
         if self.resultado is None:
-            print("Falta ajustar el modelo, usar ajustar_modelo()")
+            print("AJUSTAR EL MODELO ANTES")
         else:
             X_test = sm.add_constant(self.x_test)
             ajust_y_prob = self.resultado.predict(X_test)
-            # probabilidad del "y" ajustado
-            # lo que sigue es el "y" ajustado como binario,
-            # de acuerdo al "prob" usado:
             y_ajustado_binary = [1 if k >= prob else 0 for k in ajust_y_prob]
 
             return y_ajustado_binary, ajust_y_prob
 
-    def matriz_confusion(self, prob=0.5):
+    # TODO: anotaciones.
+    def matriz_confusion(self, prob=0.5) -> Any:
         """
         Retorna la Matriz de Confusión:
                     |tp     fp|
@@ -117,10 +112,10 @@ class RegresionLogistica:
         if self.resultado is not None:
             y_ajustado = self.ajustados_y(prob)[0]
             n = len(self.y_test)
-            tp = 0  # true positive
-            tn = 0  # true negative
-            fp = 0  # false positive
-            fn = 0  # falso negativo
+            tp = 0
+            tn = 0
+            fp = 0
+            fn = 0
             for i in range(n):
                 if y_ajustado[i] == self.y_test.iloc[i]:
                     if y_ajustado[i] == 1:
@@ -132,12 +127,12 @@ class RegresionLogistica:
                         fp = fp + 1
                     else:
                         fn = fn + 1
-            # print(f" {tp}   {fp} \n {fn}   {tn}")
             return [tp, fp, fn, tn]
         else:
-            print("Correr primero ajustados_y()")
+            print("AJUSTAR EL MODELO ANTES")
 
-    def especif_sensib(self, prob=0.5):
+    # TODO: anotaciones.
+    def especif_sensib(self, prob=0.5) -> Any:
         """
         Retorna una lista con la sensibilidad y especifisidad del modelo
         Regresión Logística, como sigue: [sensibilidad, especificidad].
@@ -153,19 +148,18 @@ class RegresionLogistica:
         sensibilidad = tp / (tp + fn)
         especificidad = tn / (fp + tn)
 
-        # print(f"La sensibilidad del modelo es: {sensibilidad}")
-        # print(f"La especificidad del modelo es: {especificidad}")
-        return [sensibilidad, especificidad]
+        return (sensibilidad, especificidad)
 
-    def curva_ROC(self, prob=0.5):
+    # TODO: anotaciones.
+    def curva_ROC(self, prob=0.5) -> Any:
         """
         prob: es el umbral de probabilidad sobre el cual se determina si una
-        respuesta es 0 o 1. Default = 0.5
+        respuesta es 0 o 1.
         """
         if self.resultado is not None:
             grid = np.linspace(0, 1, 100)
-            l1 = []  # 1-especificidad
-            l2 = []  # sensibilidad
+            l1 = []
+            l2 = []
             prediccion = self.ajustados_y(prob)[1]
             for j in grid:
                 y_pred_binary = [1 if k >= j else 0 for k in prediccion]
@@ -180,23 +174,24 @@ class RegresionLogistica:
             plt.legend()
             plt.xlabel("1-especificidad")
             plt.ylabel("sensibilidad")
-            plt.title('Curva ROC')
+            plt.title("Curva ROC")
             plt.show()
-
         else:
-            print("Falta ajustar el modelo")
+            print("AJUSTAR EL MODELO ANTES")
 
-    def predict_y(self, x_new, prob=0.5):
+    # TODO: anotaciones.
+    def predict_y(self, x_new, prob=0.5) -> Any:
         """
         x_new es una lista, con el/los valor/es que se quiere predecir.
         La funcion retorna el valor de predicciòn para un x_new.
         prob: es el umbral de probabilidad sobre el cual se determina si una
-         respuesta es 0 o 1. Default = 0.5
+         respuesta es 0 o 1.
         """
         X_new = x_new.copy()
         X_new.insert(0, 1)
         aux = np.dot(self.resultado.params, X_new)
         pred = np.exp(aux)/(1 + np.exp(aux))
+
         if pred >= prob:
             y_pred_bin = 1
         else:
@@ -204,7 +199,8 @@ class RegresionLogistica:
 
         return y_pred_bin
 
-    def auc(self, prob=0.5):
+    # TODO: anotaciones.
+    def auc(self, prob=0.5) -> Any:
         """
         Imprime la evaluacion del clasificador, teniendo en cuenta la tabla
         dada en teoría.
@@ -221,7 +217,6 @@ class RegresionLogistica:
                 sensibilidad_list.append(sensibilidad)
 
             roc_auc = auc(1-np.array(especificidad_list), sensibilidad_list)
-            # print("AUC:", roc_auc)
             if 0.90 < roc_auc <= 1:
                 print(f"El clasificador es Excelente, {roc_auc}")
             elif 0.80 < roc_auc <= 0.90:
@@ -234,39 +229,36 @@ class RegresionLogistica:
                 print(f"El clasificador es Fallido, {roc_auc}")
             else:
                 print(f"El clasificador Muy Malo, {roc_auc}")
-
         else:
-            print("Falta ajustar el modelo, usar ajustar_modelo()")
+            print("AJUSTAR EL MODELO ANTES")
 
-    def modelo_resumen(self):
+    # TODO: anotaciones.
+    def modelo_resumen(self) -> Any:
         """
         Imprime el summary del modelo ajustado
         """
         if self.resultado is not None:
             print(self.resultado.summary())
         else:
-            print("Falta ajustar el modelo, usar ajustar_modelo()")
+            print("AJUSTAR EL MODELO ANTES")
 
-    def p_valor_betas(self, b_i=0, i=1):
+    # TODO: anotaciones.
+    def p_valor_betas(self, b_i=0, i=1) -> Any:
         """
         Es una funcion que retorna el p-valor de un test de hipotesis:
         H_0: beta_i = k vs H_1 beta_i != k
-        b_i: es el numero k sobre el cual se quiere hacer el test. Por
-        default es 0.
-        i: es el indice del beta que se quiere testear, es un natural i
-        (i = 0, ..., n). Por default es 1.
+        b_i: es el numero k sobre el cual se quiere hacer el test.
+        i: es el indice del beta que se quiere testear (es un entero).
         """
         if self.resultado is None:
-            print("Falta ajustar el modelo, usar ajustar_modelo()")
+            print("AJUSTAR EL MODELO ANTES")
         else:
             res = self.resultado
             SE_est = res.bse
             coef_xi = res.params[i]
-            # valor de t observado:
             t_obs = (coef_xi - b_i) / SE_est[i]
 
-            # el pvalor:
-            X = res.model.exog  # para recuperar la matriz de diseño del modelo
+            X = res.model.exog
             grados_libertad = len(X[:, i]) - 2
             p_valor = 2 * stats.t.sf(abs(t_obs), df=grados_libertad)
 
