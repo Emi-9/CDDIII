d415cc12913326ba5bc196fe124165b72c39d71e
Emiliano <167711677+Emi-9@users.noreply.github.com>
2025-06-19 00:05:03 -0300

    Update __init__.py

--- modelos/__init__.py
+++ modelos/__init__.py
@@ -2,7 +2,7 @@ from .regresion_lineal import RegresionLineal
 from .regresion_lineal_simple import RegresionLinealSimple
 from .regresion_lineal_multiple import RegresionLinealMultiple
 from .regresion_logistica import RegresionLogistica
-from .anova import Anova.generic_anova_analysis as anova
+from .anova import anova
 
 __all__ = ["RegresionLineal", "RegresionLinealSimple",
            "RegresionLinealMultiple", "RegresionLogistica", "anova"]
